c81460132a082703b13c1e958d69f2db4f46fa9f
PravinDalavi <pravin.dalavi@gmail.com>
2021-11-26 14:31:40 +0530

    test

--- CreditQueueListener/ProcessMessages.cs
+++ CreditQueueListener/ProcessMessages.cs
@@ -109,7 +109,15 @@ namespace CreditQueueListener
 
         public string GetConnStr()
         {
-            string sConnectionString = "server = applicationsubmission.cikv7fwlsku8.ap-south-1.rds.amazonaws.com; port=3306; uid=admin; pwd=admin8910; database=CreditApproval";
+            //string sConnectionString = "server = applicationsubmission.cikv7fwlsku8.ap-south-1.rds.amazonaws.com; port=3306; uid=admin; pwd=admin8910; database=CreditApproval";
+            string Server = Environment.GetEnvironmentVariable("Server");
+            string Port = Environment.GetEnvironmentVariable("Port");
+            string UID = Environment.GetEnvironmentVariable("UID");
+            string PWD = Environment.GetEnvironmentVariable("PWD");
+            string Database = Environment.GetEnvironmentVariable("Database");
+
+            //string sConnectionString = "server = applicationsubmission.cikv7fwlsku8.ap-south-1.rds.amazonaws.com; port = 3306; uid = admin; pwd = admin8910; database = CreditApproval";
+            string sConnectionString = "server = " + Server + "; port = " + Port + "; uid = " + UID + "; pwd = " + PWD + "; database = " + Database;
             return sConnectionString;
         }
 
